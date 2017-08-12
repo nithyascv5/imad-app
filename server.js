c89b842ad51f470198493468e995b180ba1f296f
nithyascv5 <30650576+nithyascv5@users.noreply.github.com>
2017-08-11 23:54:38 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('Ariticle one', function (req, res) {
+  res.sendFile('Article one accesed here and it will be served here');
+});
+
+app.get('Ariticle two', function (req, res) {
+  res.sendFile('Article two accesed here and it will be served here');
+});
+
+  app.get('Ariticle three', function (req, res) {
+  res.sendFile('Article three accesed here and it will be served here');
+  });
+  
+  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
