ac1919860736e756a84a67b9661769951b23e004
nithyascv5 <30650576+nithyascv5@users.noreply.github.com>
2017-08-12 02:24:17 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='CHANGED NAME';
 
+//move images
+ var img= document.getElementById('img')
+ img.onclick= function()
+ {
+     img.style.marginleft='100px';
+ };
