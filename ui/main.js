1d6dd17c47b6caa77e05ecd1c51cf2071eb5a5af
nithyascv5 <30650576+nithyascv5@users.noreply.github.com>
2017-08-14 07:44:57 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ button.onclick=function(){
        }
     }
   };
-  request.open('GET','http://nithyascv5.imad.hasura-app.io/counter',true);
+  request.open('GET','http://nithyascv5.imad.hasura.app.io/counter',true);
   request.send(null);
 };
