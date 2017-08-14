921f46a443fb252f1681f44d0e55b22b9eef980e
nithyascv5 <30650576+nithyascv5@users.noreply.github.com>
2017-08-14 07:42:18 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,18 @@
 var button = document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
+    var request = new XMLHttpRequest();
   counter=counter+1;
   var span = document.getElementById('count');
+  request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.DONE){
+       if(request.status === 200){
+          var counter = request.responseText;
+
   span.innerHTML = counter.toString();
+       }
+    }
+  };
+  request.open('GET','http://nithyascv5.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
